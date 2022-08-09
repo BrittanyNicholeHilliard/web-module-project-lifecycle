c7736362129697397b0d2ddb6d3d0b7fa975eb26
timothy hoover <bnicholehilliard@gmail.com>
2022-08-09 15:09:23 -0700

    first push

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import axios from 'axios'
+import TodoList from './TodoList'
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+
+  state = {
+    todos: [], 
+    error: '', 
+    todoNameInput: '', 
+    displayCompleteds: true
+  }
+
+ onChange = (evt) =>  {
+  this.setState({...this.state, todoNameInput: evt.target.value})
+  console.log(this.state.todoNameInput)
+
+ }
+
+ fetchAllTodos() {
+  axios.get(URL)
+  .then(res => {
+    console.log(res.data.data)
+  })
+ }
+
+ componentDidMount() {
+  this.fetchAllTodos()
+ }
+
   render() {
-    return null
+    return (
+      <div>
+      <header>TODOS:</header>
+      {this.state.todos}
+      <form>
+        <input type="text" placeholder="enter task" onChange={this.onChange}/>
+        <button>Submit</button>
+      </form>
+      </div>
+    )
   }
 }
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,7 +1,17 @@
+import axios from 'axios'
 import React from 'react'
 
 export default class TodoList extends React.Component {
+
+
+
+
   render() {
-    return null
+    return (
+      <div>
+        
+      </div>
+      
+    )
   }
 }
